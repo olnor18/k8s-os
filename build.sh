08c662247d0a0ccc0238ea7ca66baffc38d2d4ca
Kristian Klausen <kristian@klausen.dk>
2022-06-22 10:33:46 +0200

    Document the code ane explain what the functions does

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ readonly orig_pwd="${PWD}"
 readonly tmpdir="$(mktemp --dry-run --directory --tmpdir="${PWD}/tmp")"
 trap "rm -rf \"${tmpdir}\"" EXIT
 
+# Helper for chrooting
 chroot() {
   mv debian/etc/resolv.conf{,.org}
   cp -L /etc/resolv.conf debian/etc/resolv.conf
@@ -13,19 +14,23 @@ chroot() {
   mv debian/etc/resolv.conf{.org,}
 }
 
+# Create output and tmpdir
 init() {
   mkdir -p "${output}" "${tmpdir}"
   if [[ -n ${SUDO_UID:-} ]] && [[ -n ${SUDO_GID:-} ]]; then
+    # Chown the directories to the user who invoked the script
     chown "${SUDO_UID}:${SUDO_GID}" "${output}" "tmp"
   fi
   cd "${tmpdir}"
 }
 
+# Bootstrap a minimal Debian system and install systemd and a kernel
 bootstrap() {
   debootstrap --variant=minbase bullseye debian https://deb.debian.org/debian/
   chroot apt-get install -y systemd linux-image-amd64
 }
 
+# Setup CRI-O and k8s (kubelet, kubeadm and kubectl)
 k8s() {
   cp -r "${orig_pwd}/files/cri-o/"* debian/
   cp -r "${orig_pwd}/files/k8s/"* debian/
@@ -33,9 +38,12 @@ k8s() {
   chroot apt-get install -y cri-o cri-o-runc kubelet kubeadm kubectl
 }
 
+# Misc stuff
 misc() {
+  # Copy init script used in the initrd
   cp "${orig_pwd}/files/init" debian/
   cp -r "${orig_pwd}/files/misc/"* debian/
+  # Copy files required for mukube-configurator
   cp -r "${orig_pwd}/files/mukube-configurator/"* debian/
   # Required by zap-ceph-disks.service
   chroot apt-get install -y --no-install-recommends gdisk
@@ -45,6 +53,8 @@ misc() {
   echo debian > debian/etc/hostname
 }
 
+# Create Unified Kernel Image (single EFI binary containing the whole system)
+# https://systemd.io/BOOT_LOADER_SPECIFICATION/#type-2-efi-unified-kernel-images
 unified_kernel_image() {
   cd debian
   find . -not \( -name "vmlinuz*" -o -name "initrd.img*" \) | cpio -o --format=newc | zstd --long > ../initrd.img
@@ -60,12 +70,15 @@ unified_kernel_image() {
     debian.efi
 }
 
+# Create raw image with a EFI System Partition (ESP) containing the EFI binary (Unified Kernel Image)
 image() {
   truncate -s "512M" "debian.img"
+  # Create partition tabel
   sgdisk --clear \
     --new 0:0:+0 --typecode=0:ef00 \
     debian.img
 
+  # Calculate the location of the first partition in the image
   local json sector_size offset block_count
   json="$(sfdisk --json debian.img)"
   sector_size="$(jq .partitiontable.sectorsize <<< "${json}")"
@@ -73,15 +86,20 @@ image() {
   block_count="$(jq .partitiontable.partitions[0].size <<< "${json}")"
   block_count="$((block_count*sector_size/1024))"
 
+  # Format the partition as a FAT filesystem
   mkfs.fat --offset "${offset}" debian.img "${block_count}"
+  # Create directories
   mmd -i debian.img@@$((offset*sector_size)) ::/EFI ::/EFI/BOOT
+  # Copy the EFI binary to the filesystem
   mcopy -i debian.img@@$((offset*sector_size)) debian.efi ::/EFI/BOOT/BOOTx64.EFI
 }
 
+# Prod image
 prod() {
   true
 }
 
+# Dev image
 dev() {
   chroot passwd -d root
   chroot apt-get install -y nano tmux htop
@@ -101,15 +119,18 @@ main() {
   for image_type in {prod,dev}; do
     cp -ar debian{,.copy}
     ${image_type}
+    # Clear out downloaded packages from the cache
     chroot apt-get clean
 
     unified_kernel_image
     image
+    # Create a text file with the list of installed packages
     chroot dpkg-query --showformat='${Package} ${Version}\n' -W > packages.txt
     mv debian.efi "${output}/k8s-os-${image_type}.efi"
     mv debian.img "${output}/k8s-os-${image_type}.img"
     mv packages.txt "${output}/k8s-os-${image_type}-packages.txt"
     if [[ -n ${SUDO_UID:-} ]] && [[ -n ${SUDO_GID:-} ]]; then
+      # Chown the artifacts to the user who invoked the script
       chown "${SUDO_UID}:${SUDO_GID}" "${output}/k8s-os-${image_type}"{.efi,.img,-packages.txt}
     fi
 
